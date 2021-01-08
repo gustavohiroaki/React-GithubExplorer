680bdf3f0e9f0d10b3f2d297a68feb53e5ac30fa
Gustavo Hiroaki <gustavohiroaki@gmail.com>
2021-01-08 13:13:54 -0300

    feat(error): error handling on Dashboard

--- src/pages/Dashboard/styles.ts
+++ src/pages/Dashboard/styles.ts
@@ -1,5 +1,9 @@
-import styled from "styled-components";
+import styled, { css } from "styled-components";
 import { shade } from "polished";
+
+interface FormProps {
+  hasError: boolean;
+}
 
 export const Title = styled.h1`
   h1 {
@@ -12,7 +16,7 @@ export const Title = styled.h1`
   }
 `;
 
-export const Form = styled.form`
+export const Form = styled.form<FormProps>`
   margin-top: 40px;
   max-width: 700px;
 
@@ -25,6 +29,13 @@ export const Form = styled.form`
     border: 0;
     border-radius: 5px 0 0 5px;
     color: #3a3a3a;
+    border: 2px solid #fff;
+
+    ${(props) =>
+      props.hasError &&
+      css`
+        border-color: #c53030;
+      `}
 
     &::placeholder {
       color: #a8a8b3;
@@ -98,4 +109,9 @@ export const Repositories = styled.div`
       color: #cbcbcb;
     }
   }
+`;
+
+export const Error = styled.span`
+  display: block;
+  color: #c53030;
 `;
